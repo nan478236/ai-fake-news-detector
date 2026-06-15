6deefc21afd79895949b2089a65aa82e5d7a622d
nan478236 <nandithahello@gmail.com>
2026-06-15 20:23:37 +0530

    Fix Railway startup

--- appy.py
+++ appy.py
@@ -38,12 +38,7 @@ def home():
         explanation=explanation
     )
 
-if __name__ == '__main__':
+    if __name__ == "__main__":
     import os
-
-if __name__ == "__main__":
-    import os
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
